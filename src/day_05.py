dba44b21eb165c8d4495f96a39d742293a344ca9
rpmartz <ryan@ryanpmartz.com>
2021-11-20 05:51:09 -0800

    refactor to generic binary search function

--- src/day_05.py
+++ src/day_05.py
@@ -1,27 +1,20 @@
 from typing import List
 
+lower_designators = {'L', 'F'}
+upper_designators = {'R', 'B'}
+allowed_designators = upper_designators.union(lower_designators)
 
-def halve_row_range(range, designator):
-    if designator not in ('F', 'B'):
-        raise Exception('Designator needs to be `F` or `B`')
+
+def binary_search(range, designator):
+    if designator not in allowed_designators:
+        raise Exception('Designator needs to be `F`, `B`, `L`, or `R`')
 
     midpoint = int(len(range) / 2)
 
-    if designator == 'F':
+    if designator in lower_designators:
         return range[0:midpoint]
     else:
         return range[midpoint:]
-
-
-def halve_column_range(cols, designator):
-    if designator not in ('L', 'R'):
-        raise Exception('Column designator needs to be `L` or `R`')
-
-    midpoint = int(len(cols) / 2)
-    if designator == 'L':
-        return cols[0:midpoint]
-    else:
-        return cols[midpoint:]
 
 
 def build_row(inclusive_start: int, exclusive_end: int) -> List[int]:
--- tests/day_05_tests.py
+++ tests/day_05_tests.py
@@ -1,49 +1,49 @@
 import unittest
 
-from src.day_05 import build_row, halve_row_range, halve_column_range
+from src.day_05 import build_row, binary_search
 
 
 class DayFiveTests(unittest.TestCase):
 
     def test_passing_bad_designator_causes_exception(self):
         with self.assertRaises(Exception):
-            halve_row_range([], 'Q')
+            binary_search([], 'Q')
 
     def test_binary_search_for_rows(self):
         # tests case FBFBBFFRLR
         original_row = build_row(0, 128)
 
-        first_half = halve_row_range(original_row, 'F')
+        first_half = binary_search(original_row, 'F')
         self.assertEqual(first_half, build_row(0, 64))
 
-        second_half = halve_row_range(first_half, 'B')
+        second_half = binary_search(first_half, 'B')
         self.assertEqual(second_half, build_row(32, 64))
 
-        third_half = halve_row_range(second_half, 'F')
+        third_half = binary_search(second_half, 'F')
         self.assertEqual(third_half, build_row(32, 48))
 
-        fourth_partition = halve_row_range(third_half, 'B')
+        fourth_partition = binary_search(third_half, 'B')
         self.assertEqual(fourth_partition, build_row(40, 48))
 
-        fifth_partition = halve_row_range(fourth_partition, 'B')
+        fifth_partition = binary_search(fourth_partition, 'B')
         self.assertEqual(fifth_partition, build_row(44, 48))
 
-        sixth_partition = halve_row_range(fifth_partition, 'F')
+        sixth_partition = binary_search(fifth_partition, 'F')
         self.assertEqual(sixth_partition, build_row(44, 46))
 
-        seventh_partition = halve_row_range(sixth_partition, 'F')
+        seventh_partition = binary_search(sixth_partition, 'F')
         self.assertEqual(seventh_partition, [44])
 
     def test_column_binary_search(self):
         columns = build_row(0, 8)
 
-        first_partition = halve_column_range(columns, 'R')
+        first_partition = binary_search(columns, 'R')
         self.assertEqual(first_partition, build_row(4, 8))
 
-        second_partition = halve_column_range(first_partition, 'L')
+        second_partition = binary_search(first_partition, 'L')
         self.assertEqual(second_partition, build_row(4, 6))
 
-        third_partition = halve_column_range(second_partition, 'R')
+        third_partition = binary_search(second_partition, 'R')
         self.assertEqual(third_partition, [5])
 
 
