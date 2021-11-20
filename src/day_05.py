264dd545341d9154ed4b0421542d6d08986dc50d
rpmartz <ryan@ryanpmartz.com>
2021-11-20 05:18:53 -0800

    unit test binary search

--- src/day_05.py
+++ src/day_05.py
@@ -1,7 +1,18 @@
+from typing import List
 
 def halve_range(range, designator):
     if designator not in ('F', 'B'):
         raise Exception('Designator needs to be `F` or `B`')
+
+    midpoint = int(len(range) / 2)
+
+    if designator == 'F':
+        return range[0:midpoint]
+    else:
+        return range[midpoint:]
+
+def build_row(inclusive_start: int, exclusive_end: int) -> List[int]:
+    return [x for x in range(inclusive_start, exclusive_end)]
 
 
 # with open('data/05.txt', 'r') as f:
--- tests/day_05_tests.py
+++ tests/day_05_tests.py
@@ -1,4 +1,4 @@
-from src.day_05 import halve_range
+from src.day_05 import build_row, halve_range
 
 import unittest
 
@@ -8,6 +8,32 @@ class DayFiveTests(unittest.TestCase):
     def test_passing_bad_designator_causes_exception(self):
         with self.assertRaises(Exception):
             halve_range([], 'Q')
+
+    def test_binary_search(self):
+        # tests case FBFBBFFRLR
+        original_row = build_row(0, 128)
+
+        first_half = halve_range(original_row, 'F')
+        self.assertEqual(first_half, build_row(0, 64))
+
+        second_half = halve_range(first_half, 'B')
+        self.assertEqual(second_half, build_row(32, 64))
+
+        third_half = halve_range(second_half, 'F')
+        self.assertEqual(third_half, build_row(32, 48))
+
+        fourth_partition = halve_range(third_half, 'B')
+        self.assertEqual(fourth_partition, build_row(40, 48))
+
+        fifth_partition = halve_range(fourth_partition, 'B')
+        self.assertEqual(fifth_partition, build_row(44, 48))
+
+        sixth_partition = halve_range(fifth_partition, 'F')
+        self.assertEqual(sixth_partition, build_row(44, 46))
+
+        seventh_partition = halve_range(sixth_partition, 'F')
+        self.assertEqual(seventh_partition, [44])
+
 
 if __name__ == '__main__':
     unittest.main()
